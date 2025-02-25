74cedf5992eda62da4dbfd8f5b663433cfd5402b
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-02-25 11:16:55 +0530

    my first commits

---
+++ retreive.sql
@@ -0,0 +1,4 @@
+USE sql_store;
+SELECT * -- '*' This mark is use to select all columns in specified table
+FROM customers -- WHERE customer_id =1    --use to get record(row) of customer_id 1
+ORDER BY first_name --sort records by using first name of customer
