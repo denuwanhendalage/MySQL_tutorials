e1d25933ff8c244202b8f1a1883bc1c059dc08e7
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-03-02 22:11:21 +0530

    tutorial 5

---
+++ join.sql
@@ -0,0 +1,9 @@
+SELECT order_id,
+    orders.customer_id,
+    first_name,
+    last_name
+FROM orders
+    JOIN customers ON orders.customer_id = customers.customer_id --we are joining the customers table to order table using customer_id 
+    --SELECT order_id,customer_id,first_name, last_name this command line has an error
+    --error is customer_id this column is in orders table and also customers table. therefore give an ambiguous error
+    --we can correct that error using order.customer_id or customers,customer_id
