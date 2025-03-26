507113f05fc08903b3b3bf4f42178978152fb7c6
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-03-26 17:03:46 +0530

    insert rows tutorials

---
+++ insertrow.sql
@@ -0,0 +1,28 @@
+INSERT INTO customers
+VALUES (
+        DEFAULT,
+        'Shashanka',
+        'Denuwan',
+        '2002-11-08',
+        NULL,
+        'negombo, Sri lanka',
+        'Negombo',
+        'CA',
+        DEFAULT
+    );
+INSERT INTO customers (
+        first_name,
+        last_name,
+        birth_date,
+        address,
+        city,
+        state
+    )
+VALUES (
+        'Shashanka',
+        'Denuwan',
+        '2002-11-08',
+        'negombo, Sri lanka',
+        'Negombo',
+        'CA'
+    );
--- union.sql
+++ union.sql
@@ -8,4 +8,28 @@ SELECT order_id,
     order_date,
     'Archived' AS status
 FROM orders
-WHERE order_date < '2019-01-01'
+WHERE order_date < '2019-01-01' -- points<2000  'bronze'
+    -- points >=2000 AND points<3000   'silver'
+    -- points >3000 'gold'
+SELECT customer_id,
+    first_name,
+    points,
+    'Bronze' AS type
+FROM customers
+WHERE points < 2000
+UNION
+SELECT customer_id,
+    first_name,
+    points,
+    'Silver' AS type
+FROM customers
+WHERE points >= 2000
+    AND points < 3000
+UNION
+SELECT customer_id,
+    first_name,
+    points,
+    'Gold' AS type
+FROM customers
+WHERE points > 3000
+ORDER BY first_name
