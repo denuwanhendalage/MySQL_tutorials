df9a99f1042706ab3773e109fdf177014da735c2
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-03-31 23:18:40 +0530

    insert rows and copy tables

---
+++ inserthirachicalrows.sql
@@ -0,0 +1,5 @@
+INSERT INTO orders (customer_id, order_date, status)
+VALUES (1, '2019-01-02', 1);
+INSERT INTO order_itemS
+VALUES (LAST_INSERT_ID(), 1, 1, 2.95),
+    (LAST_INSERT_ID(), 2, 1, 3.95)
---
+++ insertmultiplerows.sql
@@ -0,0 +1,11 @@
+INSERT INTO products(
+        name,
+        quantity_in_stock,
+        unit_price
+    )
+VALUES ('manche', 12, 20),
+    ('sunlight', 124, 1);
+INSERT INTO shippers(name)
+VALUES ('shipper1'),
+    ('shipper2'),
+    ('shipper3');
