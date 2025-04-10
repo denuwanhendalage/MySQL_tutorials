0250596e81273ba382dd4bbb216a1f3d81a96414
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-04-10 23:24:12 +0530

    update

--- copyfulltable.sql
+++ copyfulltable.sql
@@ -1,3 +1,15 @@
 CREATE TABLE orders_archived AS
 SELECT *
-FROM orders
+FROM orders USE sql_invoicing;
+CREATE TABLE invoice_archive AS
+SELECT i.invoice_id,
+    c.name AS client,
+    i.number,
+    i.invoice_total,
+    i.payment_total,
+    i.invoice_date,
+    i.payment_date,
+    i.due_date
+FROM invoices i
+    JOIN clients c USING (client_id)
+WHERE payment_date IS NOT NULL
