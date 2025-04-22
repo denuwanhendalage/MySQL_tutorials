42edf0f95fe6198a6447efe77f488950978967e3
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-04-22 22:17:02 +0530

    update and delete

--- updateusingsubqueries.sql
+++ updateusingsubqueries.sql
@@ -6,4 +6,12 @@ WHERE client_id = (
         SELECT client_id
         FROM clients
         WHERE name = 'yadel'
+    ) USE sql_invoicing;
+UPDATE invoices
+SET payment_total = invoice_total * 0.5,
+    payment_date = due_date
+WHERE client_id IN (
+        SELECT client_id
+        FROM clients
+        WHERE state IN ('CA', 'NY')
     )
