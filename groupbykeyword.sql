47eebc7fe53bf9f40d4dce7fcd6c96b9a297f56c
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-05-03 22:52:12 +0530

    groupby and having keywords

--- groupbykeyword.sql
+++ groupbykeyword.sql
@@ -3,4 +3,10 @@ SELECT client_id,
     SUM(invoice_total) AS total_sales
 FROM invoices
 GROUP BY (client_id)
+ORDER BY total_sales DESC USE sql_invoicing;
+SELECT client_id,
+    SUM(invoice_total) AS total_sales
+FROM invoices
+WHERE invoice_date BETWEEN '2019-01-01' AND '2019-07-01'
+GROUP BY (client_id)
 ORDER BY total_sales DESC
--- groupbytwocolumn.sql
+++ groupbytwocolumn.sql
@@ -5,4 +5,13 @@ SELECT c.state,
 FROM invoices i
 	JOIN clients c USING (client_id)
 GROUP BY state,
-	city
+	city USE sql_invoicing;
+-- this is second example 
+SELECT p.date,
+	pm.name AS payment_method,
+	SUM(p.amount) AS total_payment
+FROM payments p
+	JOIN payment_methods pm ON p.payment_method = pm.payment_method_id
+GROUP BY p.date,
+	payment_method
+ORDER BY p.date
