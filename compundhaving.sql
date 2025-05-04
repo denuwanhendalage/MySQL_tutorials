347a81b465d33478c0c711701f391d048feefa0d
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-05-04 22:42:25 +0530

    having keyword

---
+++ compundhaving.sql
@@ -0,0 +1,8 @@
+USE sql_invoicing;
+SELECT client_id,
+    SUM(invoice_total) AS total_sales,
+    COUNT(*) AS number_of_invoices
+FROM invoices
+GROUP BY client_id
+HAVING total_sales > 500
+    AND number_of_invoices > 5
--- having.sql
+++ having.sql
@@ -4,4 +4,16 @@ SELECT client_id,
     COUNT(*) AS number_of_invoices
 FROM invoices
 GROUP BY client_id
-HAVING total_sales > 500
+HAVING total_sales > 500 -- this is second example
+    USE sql_store;
+SELECT c.customer_id,
+    c.first_name,
+    c.last_name,
+    --     oi.quantity*oi.unit_price AS price,
+    SUM(oi.quantity * oi.unit_price) AS total_sales
+FROM customers c
+    JOIN orders o USING (customer_id)
+    JOIN order_items oi USING (order_id)
+WHERE state = 'VA'
+GROUP BY c.customer_id
+HAVING total_sales > 100
